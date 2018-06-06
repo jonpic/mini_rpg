41ebfcdaf2bde207e4e303a66a8677b582e38238
Jonathan Pickard <jonpic@gmail.com>
2018-06-06 17:15:07 -0400

    very basic functionality complete. enemies die and cannot be reselected

--- javascript/code.js
+++ javascript/code.js
@@ -5,6 +5,8 @@ var activeCharacterID = "";
 var activeCharacterOBJ = ""
 var activeEnemy = "";
 var activeEnemyOBJ = "";
+var defeatedEnemies = [];
+var charSelected = false;
 
 var char1 = {
     hitPoints: 10,
@@ -36,38 +38,39 @@ var pickChar = function() {
     $("#yes-no").append("<button type='button' class='btn-primary mr-3' id='yes-btn' value='yes';>Yes</button>");
     $("#yes-no").append("<button type='button' class='btn-primary ml-3' id='no-btn' value='no'>No</button>");
     $("#yes-btn").on("click", function (characterPicked){
-        if (playing = true && !activeEnemy) {
+        if (!charSelected) {
             $("#yes-btn").remove();
             $("#no-btn").remove();
             $(activeCharacterID).animate({ height: "300px" });
             $(activeCharacterID).animate({ width: "300px" });
             $(activeCharacterID).attr("value", "active-character");
+            charSelected = true;
             selectOpponent();
         }
-        else if (playing = true && activeEnemy) {
+        else if (charSelected) {
             $("#yes-btn").remove();
             $("#no-btn").remove();
             $(activeEnemyID).animate({ height: "250px" });
             $(activeEnemyID).animate({ width: "250px" });
             $(activeEnemyID).attr("value", "active-enemy");
+            playing = true;
             beginGame();
         }
 
     });
     $("#no-btn").on("click", function (){
-        if (!playing) {
-        activeCharacter = "";
-        $("#yes-btn").remove();
-        $("#no-btn").remove();
-        $("#instruction-text").text("Select a character");
-        console.log(activeEnemy)
+        if  (!charSelected) {
+            activeCharacter = "";
+            $("#yes-btn").remove();
+            $("#no-btn").remove();
+            $("#instruction-text").text("Select a character");
+            
         }
-        else if (playing) {
+        else if (charSelected) {
             activeEnemy = "";
             $("#yes-btn").remove();
             $("#no-btn").remove();
             $("#instruction-text").text("Select an opponent");
-            console.log(activeEnemy);
         };
     
     })
@@ -76,38 +79,43 @@ var pickChar = function() {
 
 
 var selectOpponent = function() {
-    if (!activeEnemy) {
+    if (charSelected) {
         $("#instruction-text").text("Select an opponent")
     }
 };
 
 var beginGame = function () {
-    $("#instruction-text").text("Time to battle!");
-    if (activeCharacterOBJ.hitPoints > 0 && activeEnemyOBJ.hitPoints > 0) {
-        console.log("you made it here")
+    
+    if (activeCharacterOBJ.hitPoints > 0 && activeEnemyOBJ.hitPoints > 0 && playing) {
+        $("#instruction-text").text("Time to battle!");
+        console.log(activeEnemyOBJ);
         attacker();
     }
     else if (activeCharacterOBJ.hitPoints > 0 && activeEnemyOBJ.hitPoints < 1) {
-        $("#instruction-text").text("You have defeated your opponent");
+        console.log("the enemy is dead");
+        $(activeEnemyID).animate({ height: "200px" });
+        $(activeEnemyID).animate({ width: "200px" });
+        $("#instruction-text").text("You defeated " + activeEnemy);
+        playing = false;
+        activeEnemy="";
+        activeEnemyOBJ="";
+        anotherEnemy();
     }
     else if (activeCharacterOBJ.hitPoints < 1) {
         $("#instruction-text").text("You are dead");
+        playing = false;
     };
 };
 
 var attacker = function() {
     $(".char-select-button").on("click", function() {
-        if (this.value === "active-enemy") {
+        if (this.value === "active-enemy" && playing === true) {
             activeEnemyOBJ.hitPoints -= activeCharacterOBJ.attack;
-            console.log(activeCharacterOBJ.hitPoints);
-            console.log(activeCharacterOBJ.attack);
-            console.log(activeCharacterOBJ.counterAttack);
-            console.log(activeCharacterOBJ['hitPoints']);
-            console.log(activeEnemyOBJ);
             console.log(activeEnemyOBJ.hitPoints);
             beginGame();
+    
         }
-        else if (this.value === "active-character") {
+        else if (this.value === "active-character" && playing === true) {
             console.log("don't attack yourself");
             beginGame();
         }
@@ -118,21 +126,28 @@ var attacker = function() {
     });
 };
 
+var anotherEnemy = function() {
+    $("#instruction-text").text("Select a new enemy");
+}
+
 //functions that handle the initial picking of the characters and asks the player to confirm who they selected
 $("#char-1-button").on("click", function () {
-    if (!playing) {
+    if (!activeCharacter) {
         activeCharacter = this.value;
         activeCharacterID = "#char-1-button";
         activeCharacterOBJ = char1;
-        pickChar(activeCharacterID);
+        pickChar();
         console.log(activeEnemy)
     }
-    else if (playing && !activeEnemy) {
+    else if (charSelected && !activeEnemy && char1.hitPoints > 0) {
         activeEnemy = this.value;
         activeEnemyID = "#char-1-button";
         activeEnemyOBJ = char1;
-        pickChar(activeEnemyID);
+        pickChar();
         console.log(activeEnemy);
+    }
+    else if (charSelected && !activeEnemy && char1.hitPoints < 1) {
+        $("#instruction-text").text("Please select a new opponenet")
     }
 });
 
@@ -143,12 +158,16 @@ $("#char-2-button").on("click", function () {
         activeCharacterOBJ = char2;
         pickChar(activeCharacterID);
     }
-    else if (playing && !activeEnemy) {
+    else if (charSelected && !activeEnemy && char2.hitPoints > 0) {
         activeEnemy = this.value;
         activeEnemyID = "#char-2-button";
         activeEnemyOBJ = char2;
         pickChar(activeEnemyID);
         console.log(activeEnemy);
+        
+    }
+    else if (charSelected && !activeEnemy && char2.hitPoints < 1) {
+        $("#instruction-text").text("Please select a new opponenet")
     }
 });
 
@@ -159,12 +178,15 @@ $("#char-3-button").on("click", function () {
         activeCharacterOBJ = char3;
         pickChar(activeCharacterID);
     }
-    else if (playing && !activeEnemy) {
+    else if (charSelected && !activeEnemy && char3.hitPoints > 0) {
         activeEnemy = this.value;
         activeEnemyID = "#char-3-button";
         activeEnemyOBJ = char3;
         pickChar(activeEnemyID);
         console.log(activeEnemy);
+    }
+    else if (charSelected && !activeEnemy && char3.hitPoints < 1) {
+        $("#instruction-text").text("Please select a new opponenet")
     }
 })
 
@@ -175,12 +197,15 @@ $("#char-4-button").on("click", function () {
         activeCharacterOBJ = char4;
         pickChar(activeCharacterID);
     }
-    else if (playing && !activeEnemy) {
+    else if (charSelected && !activeEnemy && char4.hitPoints > 0) {
         activeEnemy = this.value;
         activeEnemyID = "#char-4-button";
         activeEnemyOBJ = char4;
         pickChar(activeEnemyID);
         console.log(activeEnemy);
+    }
+    else if (charSelected && !activeEnemy && char4.hitPoints < 1) {
+        $("#instruction-text").text("Please select a new opponenet")
     }
 })
 
